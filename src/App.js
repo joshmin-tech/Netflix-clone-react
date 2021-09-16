dce64cab1fac7a216ee07884f8deb6bcff15c6e6
joshmin <joshminrs@gmail.com>
2021-09-16 15:58:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import {originals,action} from './urls'
+import './App.css'
+import Banner from './Components/Banner/Banner'
+import RowPost from './Components/RowPost/RowPost'
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar/>
+        <Banner/>
+        <RowPost url={originals} title='Netflix Originals' />
+        <RowPost url={action} title='Actions' isSmall />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = "d278b594e297f4470d7d04397d7e91cd";
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
